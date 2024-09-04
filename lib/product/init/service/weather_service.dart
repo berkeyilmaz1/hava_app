2d8605280a3a3d1330ef0ae427079ee0f73ce134
berke.yz <berkeyilmaz189@gmail.com>
2024-09-04 18:09:01 +0300

    chore: Refactor weather service to use latitude and longitude directly

--- lib/product/init/service/weather_service.dart
+++ lib/product/init/service/weather_service.dart
@@ -2,13 +2,12 @@ import 'package:geolocator/geolocator.dart';
 import 'package:hava_app/core/service/model/base_error_model.dart';
 import 'package:hava_app/product/init/service/model/weather_model.dart';
 import 'package:hava_app/product/init/service/model/weather_service_paths.dart';
-import 'package:hava_app/product/init/service/model/weather_service_query.dart';
 import 'package:vexana/vexana.dart';
 
 /// Weather service interface
-abstract class IWeatherServiceInterface {
+abstract class WeatherServiceInterface {
   ///Weather Service Interface
-  IWeatherServiceInterface({
+  WeatherServiceInterface({
     required INetworkManager<BaseErrorModel> networkManager,
   }) : _networkManager = networkManager;
 
@@ -17,41 +16,11 @@ abstract class IWeatherServiceInterface {
   /// this method gets weather information.
 
   Future<WeatherModel?> getCurrentWeather(Position city);
-
-  /// This method gets city location
-  Future<Position?> getCurrentCity();
 }
 
 /// Weather service
-final class WeatherService extends IWeatherServiceInterface {
-  /// Weather Service constructor method required network manager
+final class WeatherService extends WeatherServiceInterface {
   WeatherService({required super.networkManager});
-
-  @override
-  Future<Position> getCurrentCity() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) return Future.error('Location services are disabled.');
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.',
-      );
-    }
-
-    return Geolocator.getCurrentPosition();
-  }
 
   @override
   Future<WeatherModel?> getCurrentWeather(Position city) async {
@@ -59,16 +28,11 @@ final class WeatherService extends IWeatherServiceInterface {
       WeatherServicePaths.getCurrentWeather,
       parseModel: WeatherModel(),
       method: RequestType.GET,
-      queryParameters: Map.fromEntries([
-        WeatherQueries.makeQuery(
-          query: WeatherQueries.lat,
-          value: city.latitude,
-        ),
-        WeatherQueries.makeQuery(
-          query: WeatherQueries.lon,
-          value: city.longitude,
-        ),
-      ]),
+      queryParameters: {
+        'lat': city.latitude,
+        'lon': city.longitude,
+        'appid': 'APIKEY',
+      },
     );
     return response.data;
   }
