ab75cfd66e628459136c155f399897a1207ada32
berke.yz <berkeyilmaz189@gmail.com>
2024-09-05 19:49:21 +0300

    lotties added

--- lib/product/utils/constants/lottie_constants.dart
+++ lib/product/utils/constants/lottie_constants.dart
@@ -3,4 +3,13 @@ final class LottieConstants {
 
   static const loading = 'assets/lotties/loading.json';
   static const loadingError = 'assets/lotties/loading_error.json';
+  static const atmosphereDay = 'assets/lotties/atmosphere_day.json';
+  static const clearDay = 'assets/lotties/clear_day.json';
+  static const cloudSunHigh = 'assets/lotties/cloud_sun_high.json';
+  static const cloudSunLow = 'assets/lotties/cloud_sun_low.json';
+  static const drizzleDay = 'assets/lotties/drizzle_day.json';
+  static const rainWithSunDay = 'assets/lotties/rain_with_sun_day.json';
+  static const snowDay = 'assets/lotties/snow_day.json';
+  static const thunderDay = 'assets/lotties/thunder_day.json';
+  static const rainDay = 'assets/lotties/rain_day.json';
 }
